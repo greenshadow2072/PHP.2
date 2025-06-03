9b4d36d770d78a0d300b6dd4663042ddd37247e6
greenshadow2072 <kio3522022@gmail.com>
2025-06-03 10:21:46 +0800

    Update 22.bulletin_add_form.php

--- 22.bulletin_add_form.php
+++ 22.bulletin_add_form.php
@@ -1,22 +1,26 @@
 <?php
-
-    error_reporting(0);// 關閉錯誤訊息顯示
-    session_start();// 啟動 session，用來追蹤使用者登入狀態
-    // 檢查 session 中是否有 "id"，代表使用者是否已登入
+    error_reporting(0);
+    session_start();
     if (!$_SESSION["id"]) {
-        echo "請登入帳號";
+        echo "please login first";
         echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
     }
-    else{   
-     // 使用者已登入，建立與資料庫的連線
-        $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
-        if (!mysqli_query($conn, "update user set pwd='{$_POST['pwd']}' where id='{$_POST['id']}'")){
-            echo "修改錯誤";// 如果更新失敗，顯示錯誤訊息
-            echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";
-        }else{
-            echo "修改成功，三秒鐘後回到網頁";// 3 秒後跳轉回使用者頁面
-            echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";
-        }
+    else{// 顯示新增佈告的 HTML 表單
+        echo "                                                                           
+        <html>
+            <head><title>新增佈告</title></head>
+            <body>
+                <form method=post action=23.bulletin_add.php>
+                    標    題：<input type=text name=title><br>
+                    內    容：<br><textarea name=content rows=20 cols=20></textarea><br>
+                    佈告類型：<input type=radio name=type value=1>系上公告 
+                            <input type=radio name=type value=2>獲獎資訊
+                            <input type=radio name=type value=3>徵才資訊<br>
+                    發布時間：<input type=date name=time><p></p>
+                    <input type=submit value=新增佈告> <input type=reset value=清除>
+                </form>
+            </body>
+        </html>
+        ";
     }
-
 ?>
