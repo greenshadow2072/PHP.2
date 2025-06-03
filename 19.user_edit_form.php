9df20df48b5c513bc8feb690b894df478299839c
greenshadow2072 <kio3522022@gmail.com>
2025-06-03 10:15:59 +0800

    Update 19.user_edit_form.php

---
+++ 19.user_edit_form.php
@@ -0,0 +1,26 @@
+<html>
+    <head><title>修改使用者</title></head>
+    <body>
+    <?php
+    error_reporting(0);
+    session_start();
+    if (!$_SESSION["id"]) {                                                           // 若未登入
+        echo "請登入帳號";                                                             // 提示登入
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{   
+        $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+        $result=mysqli_query($conn, "select * from user where id='{$_GET['id']}'");   // 從 user 資料表中根據 URL 參數的 id 取得該使用者資料
+        $row=mysqli_fetch_array($result);                                             // 取得該筆資料的陣列
+        echo "
+        <form method=post action=20.user_edit.php>
+            <input type=hidden name=id value={$row['id']}>
+            帳號：{$row['id']}<br> 
+            密碼：<input type=text name=pwd value={$row['pwd']}><p></p>
+            <input type=submit value=修改>
+        </form>
+        ";
+    }
+    ?>
+    </body>
+</html>
